8375317e9e76e7b317a1f4a1a9acfc187eec12b0
Rauf Erk <erk.rauf@gmail.com>
2019-04-28 21:22:16 +0300

    norm mode

--- runner.js
+++ runner.js
@@ -3,7 +3,7 @@ function moveMyDiv(param) {
     // mydivData.innerText = param;
     mydivData.style.backgroundColor = `rgb(${Math.random() * 256},${Math.random() * 256},${Math.random() * 256})`;
     let widthSize = Math.round(Math.random() * 100) + 20;
-    widthSize = 100;
+    // widthSize = 100;
     mydivData.style.width = widthSize + 'px';
     mydivData.style.height = widthSize + 'px';
 
@@ -15,29 +15,43 @@ function moveMyDiv(param) {
     let xSpeed = Math.round(Math.random()) || -1;
     let ySpeed = Math.round(Math.random()) || -1;
     let rSpeed = (Math.round(Math.random()) || -1) * Math.round(Math.random() * 5);
-    // mydivData.style.transform = `rotate(${angle}deg)`;
-
-
+    mydivData.style.transform = `rotate(${angle}deg)`;
 
     var timerId = setInterval(function () {
 
-        let allDivArray = document.getElementsByTagName('div');
-        // console.log(allDivArray.length);
-        for (let i = 0; i <= allDivArray.length; i++) {
-            if (allDivArray[i] != undefined && param != allDivArray[i].id) {
-                let currentDiv = allDivArray[i];                
+        // let allDivArray = document.getElementsByTagName('div');        
+        // for (let i = 0; i <= allDivArray.length; i++) {
+        //     if (allDivArray[i] != undefined && param != allDivArray[i].id) {
+        //         let currentDiv = allDivArray[i];
 
-                currDivTop = currentDiv.style.top.slice(0, -2);
-                currDivLeft = currentDiv.style.left.slice(0, -2);
-                currDivWidth = currentDiv.style.width.slice(0, -2);                
+                
+        //         currDivTop = currentDiv.style.top.slice(0, -2);
+        //         currDivLeft = currentDiv.style.left.slice(0, -2);
+        //         currDivWidth = currentDiv.style.width.slice(0, -2);
+        //         // console.log(`${currentDiv.style.top}  ${currDivTop}    currDivWidth ${currDivWidth} ${currentDiv.style.width}`)
 
-                let horizontalMatch = currDivTop >= topPos && currDivTop <= topPos + widthSize;
-                let verticalMatch = currDivLeft >= leftPos && currDivLeft <= leftPos + widthSize;
+        //         let horizontalMatch = currDivTop >= topPos && currDivTop <= topPos + widthSize;
+        //         let verticalMatch = currDivLeft >= leftPos && currDivLeft <= leftPos + widthSize;
 
-                if (horizontalMatch&&verticalMatch) git 
-
-            }
-        }
+        //         if (horizontalMatch) {
+        //             // console.log('horizontalMatch!!!')
+        //             if (xSpeed > 0 && leftPos + widthSize > currDivLeft&& leftPos+widthSize<currDivLeft+currDivWidth) {
+        //                 console.log(` leftPos: ${leftPos}  ${widthSize}    ${currDivWidth}`);
+        //                 xSpeed = xSpeed * -1;
+        //                 // leftPos += xSpeed;
+        //             }
+        //             if (xSpeed < 0 && leftPos < currDivLeft + currDivWidth&&leftPos>currDivLeft) {
+        //                 console.log(`  ${leftPos}  ${currDivLeft}    ${currDivWidth}`);
+        //                 xSpeed = xSpeed * -1;
+        //                 // leftPos += xSpeed;
+        //             }
+        //         }
+        //         if (verticalMatch){              
+                
+                
+        //         }
+        //     }
+        // }
 
 
         const maxLeft = window.innerWidth - ballMaxSize;
@@ -72,19 +86,19 @@ function moveMyDiv(param) {
 
         leftPos += xSpeed; //x
         topPos += ySpeed;  //y
-        // angle += rSpeed // rotation
+        angle += rSpeed // rotation
 
         mydivData.style.left = leftPos + 'px';
         mydivData.style.top = topPos + 'px';
-        // mydivData.style.transform = `rotate(${angle}deg)`;
+        mydivData.style.transform = `rotate(${angle}deg)`;
 
-    }, 100);
+    }, 10);
 }
 
 function createDivs() {
     let parentElem = document.body;
     let numberOfDiv = Math.round(Math.random() * 50);
-    numberOfDiv = 1;
+    // numberOfDiv = 1;
     for (let i = 0; i <= numberOfDiv; i++) {
         let mydivData = document.createElement('div');
         mydivData.className = "box";
